c1b17f6b54ae81dc67e694148ae4e0bdf75387d9
Manisha Patel <ma9shap.gmail.com>
2019-05-13 17:11:52 +0100

    see the bike docked

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,18 +1,37 @@
 require 'DockingStation'
 
 describe DockingStation do
-  #it 'release a bike' do
   describe 'release a bike' do
-    #dockingstation = DockingStation.new
-
-    #expect(dockingstation).to respond_to(:release_bike)
-    it {is_expected.to respond_to(:release_bike)}
+    it do
+    bike = Bike.new
+    dockingstation = DockingStation.new(bike)
+    expect(dockingstation).to respond_to(:release_bike)
+    #it {is_expected.to respond_to(:release_bike)}
+end
 end
   describe 'gets a bike' do
     it do
-    docking_station = DockingStation.new
+    bike = Bike.new
+    docking_station = DockingStation.new(bike)
     bike = docking_station.release_bike
     expect(bike.working?).to eq(true)
     end
+  end
+
+  describe "dock a bike" do
+    it {
+      bike = Bike.new
+      docking_station = DockingStation.new(bike)
+
+      expect(docking_station).to respond_to(:dock)
+
+    }
+  end
+  describe 'see the bike docked' do
+    it {
+      bike = Bike.new
+      docking_station = DockingStation.new(bike)
+      expect(docking_station.dock).to eq(bike)
+    }
   end
 end
